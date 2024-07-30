06bd92c6af452d4a052b1f5f4e93fad955f73a9a
Haggai Khafizov <hafyok5777@gmail.com>
2024-07-30 17:16:17 +0300

    Change theme and colors. Add enableEdgeToEdge()

--- app/src/main/java/com/example/forskills/MainActivity.kt
+++ app/src/main/java/com/example/forskills/MainActivity.kt
@@ -3,12 +3,14 @@ package com.example.forskills
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.Surface
 import com.example.forskills.ui.theme.ForSkillsTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
         setContent {
             ForSkillsTheme {
                 Surface {
--- app/src/main/java/com/example/forskills/ui/theme/Color.kt
+++ app/src/main/java/com/example/forskills/ui/theme/Color.kt
@@ -2,9 +2,23 @@ package com.example.forskills.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val Black = Color(0xFF0C0C0C)
+val Grey1 = Color(0xFF1D1E20)
+val Grey2 = Color(0xFF242529)
+val Grey3 = Color(0xFF2F3035)
+val Grey4 = Color(0xFF3E3F43)
+val Grey5 = Color(0xFF5E5F61)
+val Grey6 = Color(0xFF9F9F9F)
+val Grey7 = Color(0xFFDBDBDB)
+val White = Color(0xFFFFFFFF)
+
+val Blue = Color(0xFF4C95FE)
+val DarkBlue = Color(0xFF00427D)
+val Green = Color(0xFF4CB24E)
+val DarkGreen = Color(0xFF1E341F)
+val Red = Color(0xFFFF5E5E)
+val Orange = Color(0xFFF36E36)
+
 
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
